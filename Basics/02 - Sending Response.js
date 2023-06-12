ab3cbfd0e11610e1837c802ff96b1c9d6850ff88
Abenezer Desta <abenezerth@gmail.com>
2023-06-12 16:59:29 +0300

    Updated 02 - sending response.js files

--- Basics/02 - Sending Response.js
+++ Basics/02 - Sending Response.js
@@ -2,6 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res)=>{
     console.log(req);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html><head><title>First page</title></head>');
+    res.write('<body><h1>test</h1></body>')
+    res.write('</html>')
+    res.end();
 });
 
 server.listen(3000);
